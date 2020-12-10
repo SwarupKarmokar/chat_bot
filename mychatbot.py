f81e201bac161506cc901d7b544377b5a03815ea
SwarupKarmokar <swarupkarmokar123@gmail.com>
2020-12-10 12:27:55 +0530

    chatbot added

---
+++ mychatbot.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import webbrowser
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'hey_buddy' in command:
+                command = command.replace('hey_buddy', '')
+                # print(command)
+    except:
+        pass
+    return command
+
+
+def run_buddy():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'tell' in command:
+        person = command.replace('tell', '')
+        info = wikipedia.summary(person, 1)
+        print('Your answer is: ',info)
+        talk(info)
+    elif 'who is' in command:
+        search_result = command.replace('who', '')
+        webbrowser.open('https://google.com/?#q=' +  search_result)
+    # elif 'tell me about'
+    
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_buddy()
